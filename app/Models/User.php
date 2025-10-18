da9fff31c0199b17c25192fdc01d16d4305164d8
enversanli <en.sanli@hotmail.com>
2025-10-19 01:44:30 +0200

    user right

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
         return "{$this->first_name} {$this->last_name}";
     }
 
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true;
+    }
+
 }
