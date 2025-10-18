ba3a80dce0132de7f6a8dc98d173db88e3093bea
enversanli <en.sanli@hotmail.com>
2025-10-19 01:53:01 +0200

    user right 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Traits\CommonTrait;
 use App\Traits\UserTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
